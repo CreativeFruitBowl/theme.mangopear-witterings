12ce289fef99b561e34d03098adef0474a19cd0e
Andi North <andi@mangopear.co.uk>
2019-01-03 13:05:01 +0000

    [Footer] Added topics bar globally

---
+++ template-partials/footer-additions.php
@@ -0,0 +1,37 @@
+<?php
+
+	/**
+	 * [Partial] Footer additions
+	 *
+	 * @category 	Core WordPress template files
+	 * @package  	mangopear-core
+	 * @author  	Andi North <andi@mangopear.co.uk>
+	 * @copyright  	2018 Mangopear creative
+	 * @license   	GNU General Public License <http://opensource.org/licenses/gpl-license.php>
+	 * @version  	1.0.0
+	 * @since   	1.0.0
+	 */
+	
+?>
+
+	<section class="c-topics">
+		<div class="o-container">
+			<header class="c-topics__header">
+				<h2 class="c-topics__title">Popular topics</h2>
+			</header>
+
+
+			<div class="c-topics__content">
+				<nav class="o-nav  o-nav--row  c-topics__nav">
+					<ul class="o-nav__list">
+						<li class="o-nav__item"><a class="o-nav__link" href="/category/buses/">Buses</a></li>
+						<li class="o-nav__item"><a class="o-nav__link" href="/category/trips/">Trips</a></li>
+						<li class="o-nav__item"><a class="o-nav__link" href="/category/railway/">Trains &amp; railways</a></li>
+						<li class="o-nav__item"><a class="o-nav__link" href="/category/the-good/">The Good</a></li>
+						<li class="o-nav__item"><a class="o-nav__link" href="/category/the-bad/">The Bad</a></li>
+						<li class="o-nav__item"><a class="o-nav__link" href="/category/the-ugly/">The Ugly</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div><!-- /.o-container -->
+	</section>
